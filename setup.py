8ca728d1b4453b46d7bcde1e0bfba93872a93f6e
Moshe Immerman <moshe.immerman@gmail.com>
2018-06-27 08:18:45 +0200

    support older versions of docker

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name = 'ansible-run', version = '2.0',
+    name = 'ansible-run', version = '2.2',
     url = 'https://www/github.com/moshloop/ansible-run',
     author = 'Moshe Immerman', author_email = 'firstname.surname@gmail.com',
-    scripts = ['ansible-run', 'ansible-test', 'ansile-role']
+    scripts = ['ansible-run', 'ansible-test', 'ansible-role']
     )
