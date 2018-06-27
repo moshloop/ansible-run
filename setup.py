effaa181c90a2ff6ff088172be5d036cdea8c0aa
Moshe Immerman <moshe.immerman@gmail.com>
2018-06-27 10:55:59 +0200

    add yq as a dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name = 'ansible-run', version = '2.2',
+    name = 'ansible-run', version = '2.3',
+    install_requires=['yq'],
     url = 'https://www/github.com/moshloop/ansible-run',
     author = 'Moshe Immerman', author_email = 'firstname.surname@gmail.com',
     scripts = ['ansible-run', 'ansible-test', 'ansible-role']
