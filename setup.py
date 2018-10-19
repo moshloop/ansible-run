95d678e1042fbc4fe24d51ccd3f8c19059eafc98
Moshe Immerman <moshe.immerman@gmail.com>
2018-10-19 15:47:13 +0200

    fix install

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name = 'ansible-run', version = '3.2',
-    install_requires=['yq'],
+    name = 'ansible-run', version = '3.3',
     url = 'https://www/github.com/moshloop/ansible-run',
     author = 'Moshe Immerman', author_email = 'firstname.surname@gmail.com',
-    scripts = ['ansible-run', 'ansible-test', 'ansible-role', 'ansible-deploy', 'ansible-vault-run']
+    scripts = ['ansible-run', 'ansible-test', 'ansible-role', 'ansible-vault-run']
     )
