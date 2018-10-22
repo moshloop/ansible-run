de95a0633e37fcbf70015a2f30448d3f408700a0
Moshe Immerman <moshe.immerman@gmail.com>
2018-10-22 05:24:40 +0200

    remove meta/main dependency installation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name = 'ansible-run', version = '3.5',
+    name = 'ansible-run', version = '3.6',
     url = 'https://www/github.com/moshloop/ansible-run',
     author = 'Moshe Immerman', author_email = 'moshe.immerman@gmail.com',
     scripts = ['ansible-run', 'ansible-test', 'ansible-role', 'ansible-vault-run']
